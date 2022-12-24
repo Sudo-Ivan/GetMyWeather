dd479ca61d9e1e088654b42bbd4247c7954ac73b
Ivan Ryan <ivan@ivanryan.dev>
2022-12-24 10:11:05 -0800

    remove sqlite database

--- cogs/owner.py
+++ cogs/owner.py
@@ -3,7 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
 
-from helpers import checks, db_manager
+from helpers import checks
 
 class Owner(commands.Cog, name="owner"):
     def __init__(self, bot):
@@ -229,93 +229,6 @@ class Owner(commands.Cog, name="owner"):
         embed = discord.Embed(
             description=message,
             color=0x9C84EF
-        )
-        await context.send(embed=embed)
-
-    @commands.hybrid_group(
-        name="blacklist",
-        description="Get the list of all blacklisted users.",
-    )
-    @checks.is_owner()
-    async def blacklist(self, context: Context) -> None:
-        """
-        Lets you add or remove a user from not being able to use the bot.
-
-        :param context: The hybrid command context.
-        """
-        if context.invoked_subcommand is None:
-            embed = discord.Embed(
-                title="Blacklist",
-                description="You need to specify a subcommand.\n\n**Subcommands:**\n`add` - Add a user to the blacklist.\n`remove` - Remove a user from the blacklist.",
-                color=0xE02B2B
-            )
-            await context.send(embed=embed)
-
-    @blacklist.command(
-        base="blacklist",
-        name="add",
-        description="Lets you add a user from not being able to use the bot.",
-    )
-    @app_commands.describe(user="The user that should be added to the blacklist")
-    @checks.is_owner()
-    async def blacklist_add(self, context: Context, user: discord.User) -> None:
-        """
-        Lets you add a user from not being able to use the bot.
-
-        :param context: The hybrid command context.
-        :param user: The user that should be added to the blacklist.
-        """
-        user_id = user.id
-        if await db_manager.is_blacklisted(user_id):
-            embed = discord.Embed(
-                title="Error!",
-                description=f"**{user.name}** is not in the blacklist.",
-                color=0xE02B2B
-            )
-            await context.send(embed=embed)
-            return
-        total = await db_manager.add_user_to_blacklist(user_id)
-        embed = discord.Embed(
-            title="User Blacklisted",
-            description=f"**{user.name}** has been successfully added to the blacklist",
-            color=0x9C84EF
-        )
-        embed.set_footer(
-            text=f"There are now {total} {'user' if total == 1 else 'users'} in the blacklist"
-        )
-        await context.send(embed=embed)
-
-    @blacklist.command(
-        base="blacklist",
-        name="remove",
-        description="Lets you remove a user from not being able to use the bot.",
-    )
-    @app_commands.describe(user="The user that should be removed from the blacklist.")
-    @checks.is_owner()
-    async def blacklist_remove(self, context: Context, user: discord.User) -> None:
-        """
-        Lets you remove a user from not being able to use the bot.
-
-        :param context: The hybrid command context.
-        :param user: The user that should be removed from the blacklist.
-        """
-        user_id = user.id
-        if not await db_manager.is_blacklisted(user_id):
-            embed = discord.Embed(
-                title="Error!",
-                description=f"**{user.name}** is already in the blacklist.",
-                color=0xE02B2B
-            )
-            await context.send(embed=embed)
-            return
-        total = await db_manager.remove_user_from_blacklist(user_id)
-        embed = discord.Embed(
-            title="User removed from blacklist",
-            description=f"**{user.name}** has been successfully removed from the blacklist",
-            color=0x9C84EF
-        )
-        embed.set_footer(
-            text=f"There are now {total} {'user' if total == 1 else 'users'} in the blacklist"
         )
         await context.send(embed=embed)
 
