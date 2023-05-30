2121dfb3d5ac51527f70f1965d4cb92e96c0fde8
Sudo-Ivan <61393936+Sudo-Ivan@users.noreply.github.com>
2023-05-30 01:59:07 -0500

    update

--- cogs/converter.py
+++ cogs/converter.py
@@ -1,4 +1,5 @@
 import math
+import discord
 from discord.ext import commands
 
 class Converter(commands.Cog):
--- cogs/fuelcalcs.py
+++ cogs/fuelcalcs.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 
 class FuelCalc(commands.Cog):
